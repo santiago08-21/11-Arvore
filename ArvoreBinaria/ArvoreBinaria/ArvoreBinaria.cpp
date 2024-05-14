343f7a940a77bfbcda158365145e59c6b44f41e6
pedro santiago <pedrosantiagopds@gmail.com>
2024-05-14 10:53:13 -0300

    santiago att11

--- ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
@@ -181,7 +181,24 @@ void buscarElementoArvore(NO* no, int valor)
 {
 
 
+	if (no == NULL) {
+		cout << "Valor nao encontrado:" << endl;
+		return;
+	}
+	else if (no->valor == valor) {
+		cout << "Valor encontrado:" << no->valor << endl;
+		return;
+	}
+	else if (no->valor > valor) {
+
+		return buscarElementoArvore(no->esq, valor);
+	}
+	else {
+		return buscarElementoArvore(no->dir, valor);
+	}
 }
+
+
 
 
 
